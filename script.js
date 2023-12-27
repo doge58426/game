548e692feba8b5e7d660881dd97a57a43eb0ffdd
doge58426 <95219562+doge58426@users.noreply.github.com>
2023-12-27 14:17:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,87 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-    event.stopPropagation();
+document.addEventListener('DOMContentLoaded', function () {
     let focusPass = false;
-    let focusUsername =false;
-    
-    const username = document.getElementById("username");
-    const password = document.getElementById("password");
+    let focusUsername = false;
+    let toggleLogger = true; // all good here
+    const logButton = document.getElementById("gameLoss"); 
+    const usernameField = document.getElementById("username");
+    const passwordField = document.getElementById("password");
     const signin = document.getElementById("signin");
     const logger = document.getElementById("logger");
+    const gamesLogged = document.getElementById("gamesLogged");
+    var username;
+    var password;
 
-
-
-    password.addEventListener("focus", function(event) {
+    passwordField.addEventListener("focus", function () {
         focusPass = true;
-        event.target.style.background = "pink";
     });
 
-    password.addEventListener("focusout", function(event) {
+    passwordField.addEventListener("focusout", function () {
         focusPass = false;
-        event.target.style.background = "aliceblue";
     });
 
-    username.addEventListener("focus", function(event) {
+    usernameField.addEventListener("focus", function () {
         focusUsername = true;
-        event.target.style.background = "pink";
     });
 
-    username.addEventListener("focusout", function(event) {
+    usernameField.addEventListener("focusout", function () {
         focusUsername = false;
-        event.target.style.background = "aliceblue";
     });
 
-    function changeDisplay(){
-        signin.style.display="none";
-        logger.style.display="block";      
+    logButton.addEventListener("click", function (event) {
+        event.target.style.background="red";
+        log();
+    });
+
+    function toggleDisplay() {
+        if (toggleLogger) {
+            signin.style.display = "none";
+            logger.style.display = "block";
+            toggleLogger = false;
+            return;
+        }
+        if (!toggleLogger) {
+            signin.style.display = "block";
+            logger.style.display = "none";
+            toggleLogger = true;
+            return;
+        }
+        return;
     }
 
-    document.addEventListener('keydown', function(event) {
+    function getUsernamePassword(){
+        globalThis.username = document.getElementById("username").value;
+        globalThis.password = document.getElementById("password").value;
+        return;
+    }   
+    
+    function log(){
+        let log = document.createElement("p");
+        log.innerHTML = `⁣⁣⁣⁣⁣  ${document.getElementById("username").value} lost the game at: ${new Date().toLocaleTimeString({'hour12': false})}`
+        gamesLogged.appendChild(log);
+    }
+    
+    document.addEventListener('keydown', function (event) {
+
         var name = event.key;
-        if (focusPass || focusUsername){
+        
+        if (focusPass || focusUsername) {
             if (name === 'Enter') {
-                changeDisplay();
+                getUsernamePassword();
+                
+                toggleDisplay();
                 focusPass = false;
-                focusUsername =false;
+                focusUsername = false;
                 return;
             }
         }
+
+        if (name === "Escape"){
+            if (!toggleLogger){
+                toggleDisplay();
+                return;
+            }
+        }
+
     });
 
 
